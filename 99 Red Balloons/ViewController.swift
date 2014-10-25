fc8a1728a4b11a3fb9fda636fee8a7470e47b64d
Scott Brady <scott.brady@cfwebtools.com>
2014-10-25 17:42:38 -0500

    Initial Commit

---
+++ 99 Red Balloons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  99 Red Balloons
+//
+//  Created by Scott Brady on 10/25/14.
+//  Copyright (c) 2014 Spider Monkey Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
